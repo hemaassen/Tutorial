758e70abf2cb17ce9b83c7afeafb9ae1ca09a98d
hemaassen <helgemaassen@yaoo.de>
2016-09-05 11:23:22 +0200

    10:13

--- app/src/main/java/com/example/helge/tutorial/MainActivity.java
+++ app/src/main/java/com/example/helge/tutorial/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.helge.tutorial;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
